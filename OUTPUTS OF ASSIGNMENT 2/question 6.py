1ec93fb6380b93ef08e33c01e79ece74e81c11bc
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:16:06 +0530

    Add files via upload

---
+++ OUTPUTS OF ASSIGNMENT 2/question 6.py
@@ -0,0 +1,11 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/OUTPUTS OF ASSIGNMENT 2/question 6.py ==============
+Enter 1st length
+23
+Enter 2nd length
+65
+Enter 3rd length
+12
+No
