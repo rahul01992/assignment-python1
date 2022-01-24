b7f6a227867682058b6b13ad5768cda548fc5d93
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:13:58 +0530

    Add files via upload

---
+++ OUTPUTS OF ASSIGNMENT 2/question 5.py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/OUTPUTS OF ASSIGNMENT 2/question 5.py ==============
+Enter a string
+my name is onkar ram
+String is  my name is onkar ram
+Yes
