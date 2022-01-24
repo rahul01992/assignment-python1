143d3d3af5d608603d6eead453af97b84fc83d62
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:05:09 +0530

    Add files via upload

---
+++ OUTPUTS OF ASSIGNMENT 2/question 2.py
@@ -0,0 +1,7 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================================================================== RESTART: D:/OUTPUTS OF ASSIGNMENT 2/question 2.py ==================================================================
+Hey Onkar Ram is here!
+My SID is 21103123.
+I am from CSE department and my cgpa is 9.9.
