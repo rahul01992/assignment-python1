0cd547edc68ce955754efae42080ce802ac15997
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:11:14 +0530

    Add files via upload

---
+++ question 4.py
@@ -0,0 +1,14 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/OUTPUTS OF ASSIGNMENT 2/question 4.py ==============
+Enter 1st number
+23
+Enter 2nd number
+65
+Enter 3rd number
+13
+23
+65
+13
+65  is greater then among three number
