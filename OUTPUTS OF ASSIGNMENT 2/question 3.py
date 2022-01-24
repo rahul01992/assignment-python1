d06414a29af1ef1f6efa5519cf7d5b929f406888
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:08:51 +0530

    Add files via upload

---
+++ OUTPUTS OF ASSIGNMENT 2/question 3.py
@@ -0,0 +1,9 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/OUTPUTS OF ASSIGNMENT 2/question 3.py ==============
+a & b is 8
+a | b is 58
+a ^ b is 50
+a<<2: 224 and b<<2 40
+a>>2: 14 and b>>4 0
