793abeecacac0a686f4a5f64bfa0f15fcba86bbc
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-01-24 17:00:42 +0530

    Add files via upload

---
+++ OUTPUTS OF ASSIGNMENT 2/question 1.py
@@ -0,0 +1,18 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Pythen 37/out question 1.py ===================
+Enter the string
+python is a case sensitive language
+python is a case sensitive language
+
+The length of string is  35
+
+The reverce string is  egaugnal evitisnes esac a si nohtyp
+
+new string is:   a case sensitive
+
+ The replaced string is  python is object oriented language
+
+The index of 'a' in given string is:  10
+The string after removing spaces is  pythonisacasesensitivelanguage
