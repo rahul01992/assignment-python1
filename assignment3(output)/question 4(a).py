da99393d9d0b54c268529016e8d58896eda04052
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-11 12:26:19 +0530

    Add files via upload

---
+++ assignment3(output)/question 4(a).py
@@ -0,0 +1,8 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/assignment3(output)/question 4.py ================
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent performance
+
+
---
+++ assignment3(output)/question 5.py
@@ -0,0 +1,10 @@
+x = 6
+for i in range(x):
+
+    for j in range(i):
+        print(' ', end='')
+    # printing alphabet
+    for j in range(2*(x-i)-1):
+        print(chr(65 + j), end='')  #ASCII value of A=65,B=66,C=67,D=68,E=69,F=70,G=71,H=72,I=73,J=74,K=75
+    print()
+print("\n")
