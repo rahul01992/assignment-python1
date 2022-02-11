d262fb7536ec7bec4e8322a92324a7f21c3d2909
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-11 12:39:42 +0530

    Add files via upload

---
+++ ASSIGNMENT 3 (OUTPUT)/question 8(a).py
@@ -0,0 +1,9 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/assignment3(output)/question 8.py ================
+a part : {1, 2, 3, 4, 8, 9}
+b part : {1, 2, 3, 4, 67, 8, 9, 12, 77, 45}
+c part : {5, 7}
+d part : {1, 3, 6, 9, 10}
+e part is {10, 6}
