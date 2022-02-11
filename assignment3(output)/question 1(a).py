baa4fbe6c4e210a6478599e9ca8a7163b4d3151b
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-11 12:29:59 +0530

    Delete assignment3(output) directory

--- assignment3(output)/question 1(a).py
+++
@@ -1,8 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-================= RESTART: D:/assignment3(output)/question 1.py ================
-ENTER ANY STRING: Hello my name is onkar
-{'Hello': 1, 'my': 1, 'name': 1, 'is': 1, 'onkar': 1}
-
-
