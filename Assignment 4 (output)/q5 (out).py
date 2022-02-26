bc3ec1b694a332df3fd40c99cd17c17efe59d575
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-26 20:13:24 +0530

    Delete q5 (out).py

--- Assignment 4 (output)/q5 (out).py
+++
@@ -1,7 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-==================== RESTART: D:/Assignment 4/Question 5.py ====================
-Answere number 5
-(a) The updated salary of Muskaan is 70000 
-(b) Record of Viren deleted
