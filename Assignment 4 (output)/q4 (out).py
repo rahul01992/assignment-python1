5ba627486a89b95b45521054f3c8300274f5f7ff
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-26 20:13:16 +0530

    Delete q4 (out).py

--- Assignment 4 (output)/q4 (out).py
+++
@@ -1,8 +0,0 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-==================== RESTART: D:/Assignment 4/Question 4.py ====================
-Answere number 4
-Object created
-The name of the student it Onkar and SID is 21103123.
-Object deleted
