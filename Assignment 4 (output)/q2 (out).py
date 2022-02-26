ef091e5be70df282e5b0d368ef2031ef2488e081
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2022-02-26 20:15:14 +0530

    Add files via upload

---
+++ Assignment 4 (output)/q2 (out).py
@@ -0,0 +1,26 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: D:/Assignment 4/Question 2.py ====================
+Answere number 2
+Enter the size of pascals triangle 6
+By use of function
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+By Use of Recursion
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+KeyboardInterrupt
---
+++ Assignment 4 (output)/q6 (out).py
@@ -0,0 +1,9 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:/Assignment 4/Question 6(new).py =================
+Answere number 6
+Enter the word: lead
+Enter a new meaningful word using the exact same letters to test your friendship: deal
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
